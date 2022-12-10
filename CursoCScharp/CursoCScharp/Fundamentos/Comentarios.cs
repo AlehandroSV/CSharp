9842a6709fed0b2f14f93218b0a90319bbfeea4a
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-12-10 20:58:23 -0300

    Fundamentos do C#

---
+++ CursoCScharp/CursoCScharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    public class Comentarios {
+        public static void Executar() {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor");
+
+            /// <summary>
+            /// 
+            /// </summary>
+            Console.WriteLine("O C# tem o XML Comments");
+
+            /*
+             * Esse é um comentário
+             * de múltiplas linhas...
+             */
+            Console.WriteLine("Comentários de multiplas linhas");
+        }
+    }
+}
---
+++ CursoCScharp/CursoCScharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentario - Fundamentos", Comentarios.Executar},
+                {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência - Fundamentos", Inferencia.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
