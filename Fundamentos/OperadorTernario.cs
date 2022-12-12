92392474eb44a1f36245a19361b223b44ceb9765
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-12-12 12:16:23 -0300

    Aula 34 - Operador Ternário (Fundamentos - Fim)

---
+++ Fundamentos/OperadorTernario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class OperadorTernario {
+        public static void Executar() {
+            // var nota = 7.0;
+            Console.Write("Qual a nota do seu aluno? ");
+            int nota = int.Parse(Console.ReadLine());
+
+            var resultado = nota >= 7.0 ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
+}
--- Fundamentos/OperadoresLogicos.cs
+++ Fundamentos/OperadoresLogicos.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 namespace CursoCSharp.Fundamentos {
     internal class OperadoresLogicos {
         public static void Executar() {
+            var executouTrabalho1 = true;
+            var executouTrabalho2 = false;
 
+            var comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine($"Comprou a Tv 50? {comprouTv50}");
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine($"Comprou o sorvete? {comprouSorvete}");
+            Console.WriteLine($"Mais saudável? {!comprouSorvete}");
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine($"Comprou Tv de 32? {comprouTv32}");
         }
     }
 }
