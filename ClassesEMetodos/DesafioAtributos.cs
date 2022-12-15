58101862a6e57ab711edda90c3c67e7b5f88ff82
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-12-15 15:50:31 -0300

    Aula 53 - Desafio Acessar Atributo (Classes e Métodos)

---
+++ ClassesEMetodos/DesafioAtributos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+    internal class DesafioAtributos {
+        int a = 10;
+
+        public static void Executar() {
+            // Acessar variável 'a' dentro do método Executar!
+
+            // Minha resolução
+            var b = new DesafioAtributos();
+            Console.WriteLine(b.a);
+
+            // Resolução da aula
+            DesafioAtributos desafio = new DesafioAtributos();
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
---
+++ ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class CalculadoraEstatica {
+        // Método de instância!
+        public int Soma(int a, int b) {
+            return a + b;
+        }
+
+        // Método de Classe ou Método estático!
+        public static int Multiplicar(int a, int b) {
+            return a * b;
+        }
+    }
+    
+    internal class MetodosEstaticos {
+        public static void Executar() {
+            var resultado = CalculadoraEstatica.Multiplicar(2, 2);
+            Console.WriteLine(resultado);
+
+            CalculadoraEstatica calc = new CalculadoraEstatica();
+            Console.WriteLine($"Somar {calc.Soma(2, 2)}");
+        }
+    }
+}
