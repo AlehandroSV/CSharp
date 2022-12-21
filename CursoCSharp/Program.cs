5c49a18a3223c306891e8675602030fd66c57b4b
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-12-21 11:00:49 -0300

    Aula 86 - Sealed - Orientação a Objeto (Fim)

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -70,6 +70,10 @@ namespace CursoCSharp {
                 { "Herança - Orientação a Objetos", Heranca.Executar },
                 { "Construtor This - Orientação a Objetos", ConstrutorThis.Executar },
                 { "Encapsulamento - Orientação a Objetos", OO.Encapsulamento.Executar },
+                { "Polimorfismo - Orientação a Objetos", Polimorfismo.Executar },
+                { "Abstract - Orientação a Objetos", Abstract.Executar },
+                { "Interface - Orientação a Objetos", Interface.Executar },
+                { "Sealed - Orientação a Objetos", Sealed.Executar },
             });
 
             central.SelecionarEExecutar();
